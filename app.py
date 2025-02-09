ff6ab5cb68bb718f683934bfe3ca35f7996c949e
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2025-02-09 12:58:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ st.write("This app uses OpenVINO for optimizing multimodal chatbot models.")
 
 # Check if a converted model exists
 if st.button("Check Converted Model"):
-    if Path("ov_nano_llava_helper.py").exists():
+    if Path("cmd_helper.py").exists():
     
         st.success("Converted model exists!")
     else:
